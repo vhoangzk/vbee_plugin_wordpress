e46dc374df647955dbbbdba437a9a3b68dd97e8b
hoangvv <vvhoangzk@gmail.com>
2021-06-24 09:09:55 +0700

    up

--- includes/vbee-api-connect-class.php
+++ includes/vbee-api-connect-class.php
@@ -54,26 +54,6 @@ class VbeeApiClass {
         $this->dataApi['content'] = $content;
         $this->dataApi['httpCallback'] = get_the_permalink($id);
         $urlApi = $this->options['address'];
-       
-        /*$curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $urlApi,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => json_encode($this->dataApi),
-            CURLOPT_HTTPHEADER => array(
-                "Content-Type: application/json"
-            ),
-        ));
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);*/
 
         $args = array(
             'body'        => json_encode($this->dataApi),
